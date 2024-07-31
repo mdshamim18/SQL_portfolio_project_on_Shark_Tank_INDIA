4a1e01ef03e5eec4b7cd15548ca4c5f86e39f96e
Md Shamim <124916800+mdshamim18@users.noreply.github.com>
2024-07-31 11:25:17 +0530

    SQL Queries with the questions

--- SQL Queries with the questions.sql
+++ SQL Queries with the questions.sql
@@ -1,121 +1,198 @@
-select * from md..data
+-- 1. Retrieve all data from the table
+SELECT * FROM sti;
 
--- total episodes in the Shark Tank India
+-- 2. Total episodes in the Shark Tank India
+SELECT COUNT(DISTINCT episode_number) FROM sti;
 
-select max(epno) from md..data
-select count(distinct epno) from md..data
+-- 3. Total pitches pitched by the startups
+SELECT COUNT(DISTINCT startup_number) FROM sti;
 
--- total pitches pitched by the startups
+-- 4. Total male contestants in the Shark Tank India
+SELECT SUM(male) FROM sti;
 
-select count(distinct brand) from md..data
+-- 5. Total female contestants in the Shark Tank India
+SELECT SUM(female) FROM sti;
 
--- total pitches which are converted into deal
+-- 6. Success rate of the startups
+SELECT 
+    COUNT(*) AS total_startups,
+    SUM(deal_offered) AS startups_with_deals,
+    (SUM(deal_offered) * 100.0 / COUNT(*)) AS success_rate
+FROM sti;
 
-select cast(sum(a.converted_not_converted) as float) /cast(count(*) as float) 
-from (select amountinvestedlakhs , case when amountinvestedlakhs>0 then 1 else 0 end as converted_not_converted from md..data) a
+-- 7. Top 5 sectors with the most investment
+SELECT TOP 5 sector, SUM(deal_amount_lakhs) AS TotalInvestment
+FROM sti
+GROUP BY sector
+ORDER BY TotalInvestment DESC;
 
--- total male contestants in the Shark Tank India
+-- 8. Average amount asked vs average amount invested
+SELECT 
+    AVG(startup_ask_amount_lakhs) AS average_ask_amount,
+    AVG(deal_amount_lakhs) AS average_invested_amount
+FROM sti
+WHERE deal_offered = 1;
 
-select sum(male) from md..data
+-- 9. Total investment of the individual sharks
+SELECT 
+    'Aman' AS shark, SUM(aman_invested) AS total_investments_in_lakhs FROM sti
+UNION
+SELECT 
+    'Anupam', SUM(anupam_invested) FROM sti
+UNION
+SELECT 
+    'Ashneer', SUM(ashneer_invested) FROM sti
+UNION
+SELECT 
+    'Ghazal', SUM(ghazal_invested) FROM sti
+UNION
+SELECT 
+    'Namita', SUM(namita_invested) FROM sti
+UNION
+SELECT 
+    'Peyush', SUM(peyush_invested) FROM sti
+UNION
+SELECT 
+    'Vineeta', SUM(vineeta_invested) FROM sti;
 
--- total female contestants in the Shark Tank India
 
-select sum(female) from md..data
+-- 10. Total invested amount and average deal size by sector-wise
+SELECT 
+    sector,
+    COUNT(*) AS total_startups,
+    SUM(deal_amount_lakhs) AS total_invested_amount,
+    AVG(deal_amount_lakhs) AS average_deal_size
+FROM sti
+GROUP BY sector
+ORDER BY total_invested_amount DESC;
 
--- gender ratio in the Shark Tank India
+-- 11. Geographical analysis
+SELECT 
+    location,
+    COUNT(*) AS total_startups,
+    SUM(deal_offered) AS startups_with_deals,
+    (SUM(deal_offered) * 100.0 / COUNT(*)) AS success_rate
+FROM sti
+GROUP BY location;
 
-select sum(female)/sum(male) from md..data
+-- 12. Episode-wise trends
+SELECT 
+    episode_number,
+    COUNT(*) AS total_deals,
+    SUM(deal_amount_lakhs) AS total_investments
+FROM sti
+WHERE deal_offered = 1
+GROUP BY episode_number
+ORDER BY total_investments DESC;
 
--- total invested amount by all the Sharks
+-- 13. Deals made by multiple sharks
+SELECT 
+    brand_name, sector, deal_amount_lakhs, sharks_offering
+FROM sti
+WHERE sharks_offering > 1;
 
-select sum(amountinvestedlakhs) from md..data
+-- 14. Identify episodes where deals were made with more than 3 team members
+SELECT episode_number, brand_name, (male + female) AS team_members
+FROM sti
+WHERE (male + female) > 3;
 
--- avg equity taken by all the Sharks
+-- 15. Number of deals and total investment per sector
+SELECT 
+    sector,
+    COUNT(*) AS total_deals,
+    SUM(deal_amount_lakhs) AS total_investment
+FROM sti
+WHERE deal_offered = 1
+GROUP BY sector
+ORDER BY total_deals DESC;
 
-select avg(a.equitytakenp) from
-(select * from md..data where equitytakenp>0) a
+-- 16. Calculate the average asked amount by all entrepreneurs
+SELECT 
+    AVG(startup_ask_amount_lakhs) AS AvgAmountAsked
+FROM sti;
 
--- highest deal given by the Shark
+-- 17. Calculate the total number of male and female contestants without considering their average age
+SELECT 
+    SUM(male) AS TotalMaleContestants,
+	SUM(female) AS TotalFemaleContestants
+FROM sti;
 
-select max(amountinvestedlakhs) from md..data 
+-- 18. Retrieve the episode number and brand name for episodes where deals were made
+SELECT episode_number, brand_name
+FROM sti
+WHERE deal_offered IS NOT NULL;
 
--- highest equity taken by the Shark
+-- 19. Find the top 5 locations with the highest average amount invested (in lakhs)
+SELECT TOP 5 location, AVG(deal_amount_lakhs) AS AvgInvestment
+FROM sti
+GROUP BY location
+ORDER BY AvgInvestment DESC;
 
-select max(equitytakenp) from md..data
+-- 20. List the brands that received investments from all the sharks (Ashneer, Namita, Anupam)
+SELECT brand_name
+FROM sti
+WHERE ashneer_invested IS NOT NULL
+AND namita_invested IS NOT NULL
+AND anupam_invested IS NOT NULL;
 
--- total startups having at least one women
+-- 21. Calculate the average equity taken by each sector
+SELECT sector, AVG(deal_equity) AS AvgEquityTaken
+FROM sti
+GROUP BY sector
+ORDER BY AvgEquityTaken DESC;
 
-select sum(a.female_count) startups having at least women 
-from (select female,case when female>0 then 1 else 0 end as female_count from md..data) a
+-- 22. Calculate the total investment amount made by each shark individually
+SELECT SharkName, SUM(amount_invested_lakhs) AS TotalInvestment
+FROM
+    (
+        SELECT episode_number, brand_name, 'Ashneer' AS SharkName, ashneer_invested AS amount_invested_lakhs FROM sti WHERE ashneer_invested IS NOT NULL
+        UNION ALL
+        SELECT episode_number, brand_name, 'Namita' AS SharkName, namita_invested AS amount_invested_lakhs FROM sti WHERE namita_invested IS NOT NULL
+        UNION ALL
+        SELECT episode_number, brand_name, 'Anupam' AS SharkName, anupam_invested AS amount_invested_lakhs FROM sti WHERE anupam_invested IS NOT NULL
+        UNION ALL
+        SELECT episode_number, brand_name, 'Ghazal' AS SharkName, ghazal_invested AS amount_invested_lakhs FROM sti WHERE ghazal_invested IS NOT NULL
+        UNION ALL
+        SELECT episode_number, brand_name, 'Peyush' AS SharkName, peyush_invested AS amount_invested_lakhs FROM sti WHERE peyush_invested IS NOT NULL
+        UNION ALL
+        SELECT episode_number, brand_name, 'Vineeta' AS SharkName, vineeta_invested AS amount_invested_lakhs FROM sti WHERE vineeta_invested IS NOT NULL
+    ) AS CombinedInvestments
+GROUP BY SharkName;
 
--- pitches converted into deal which are having at least one woman
+-- 23. Find episodes where the amount invested by Ashneer is more than the total amount invested by all other sharks combined
+SELECT episode_number, brand_name
+FROM sti
+WHERE ashneer_invested > (namita_invested + anupam_invested);
 
-select sum(b.female_count) 
-from(select case when a.female>0 then 1 else 0 end as female_count ,a.*
-from (
-(select * from md..data where deal!='No Deal')) a)b
+-- 24. Determine the average equity asked by entrepreneurs from different locations, excluding episodes where no deals were made
+SELECT location, AVG(deal_equity) AS AvgEquityAsked
+FROM sti
+WHERE deal_offered IS NOT NULL
+GROUP BY location;
 
--- avg team members from all over the startups
+-- 25. Ranking deals by the investment wise taken from the sharks
 
-select avg(teammembers) from md..data
+SELECT episode_number,
+       brand_name,
+	   deal_amount_lakhs,
+	   sector,
+       DENSE_RANK() over (ORDER BY deal_amount_lakhs desc) as investment_ranking
+FROM sti;
 
--- average amount invested per deal 
+-- 26. Third highest investment taken from the shark
 
-select avg(a.amountinvestedlakhs) amount_invested_per_deal 
-from(select * from md..data where deal!='No Deal') a
-
--- avg age group of contestants
-
-select avgage,count(avgage) cnt from md..data group by avgage order by cnt desc
-
--- location group of contestants
-
-select location,count(location) cnt from md..data group by location order by cnt desc
-
--- sector group of contestants
-
-select sector,count(sector) cnt from md..data group by sector order by cnt desc
-
--- partner deals
-
-select partners,count(partners) cnt from md..data  where partners!='-' group by partners order by cnt desc
-
--- making the matrix
-
-select * from md..data
-
-select 'Ashnner' as keyy,count(ashneeramountinvested) from md..data where ashneeramountinvested is not null
-
-select 'Ashnner' as keyy,count(ashneeramountinvested) from md..data where ashneeramountinvested is not null AND ashneeramountinvested!=0
-
-SELECT 'Ashneer' as keyy,SUM(C.ASHNEERAMOUNTINVESTED),AVG(C.ASHNEEREQUITYTAKENP) 
-FROM (SELECT * FROM md..data  WHERE ASHNEEREQUITYTAKENP!=0 AND ASHNEEREQUITYTAKENP IS NOT NULL) C
-
-select m.keyy,m.total_deals_present,m.total_deals,n.total_amount_invested,n.avg_equity_taken from
-
-(select a.keyy,a.total_deals_present,b.total_deals from(
-
-select 'Ashneer' as keyy,count(ashneeramountinvested) total_deals_present from md..data where ashneeramountinvested is not null) a
-
-inner join (
-select 'Ashneer' as keyy,count(ashneeramountinvested) total_deals from md..data 
-where ashneeramountinvested is not null AND ashneeramountinvested!=0) b 
-
-on a.keyy=b.keyy) m
-
-inner join 
-
-(SELECT 'Ashneer' as keyy,SUM(C.ASHNEERAMOUNTINVESTED) total_amount_invested,
-AVG(C.ASHNEEREQUITYTAKENP) avg_equity_taken
-FROM (SELECT * FROM md..data  WHERE ASHNEEREQUITYTAKENP!=0 AND ASHNEEREQUITYTAKENP IS NOT NULL) C) n
-
-on m.keyy=n.keyy
-
--- which is the startup in which the highest amount has been invested in each domain/sector
-
-select c.* from 
-(select brand,sector,amountinvestedlakhs,rank() over(partition by sector order by amountinvestedlakhs desc) rnk 
-
-from md..data) c
-
-where c.rnk=1
+SELECT 
+    episode_number,
+    brand_name,
+    deal_amount_lakhs,
+	sector
+FROM (
+    SELECT 
+        episode_number,
+        brand_name,
+        deal_amount_lakhs,
+		sector,
+        DENSE_RANK() OVER (ORDER BY deal_amount_lakhs DESC) AS rnk
+    FROM sti
+) AS ranked
+WHERE rnk=3;
